81e36273ec1b54a969c4ee5e9cf9d7989500368b
Pratik <pratik@Pratiks-MacBook-Pro-2.local>
2015-03-19 23:57:10 -0700

    Populated header fields with name and contact details

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -30,16 +30,18 @@ var work = {
         "employer": "TAG Employer Services",
         "title": "Web Developer Intern",
         "location": "Phoenix,Arizona",
-        "dates": "Aug 2014 to Jan 2015"
+        "dates": "Aug 2014 to Jan 2015",
+        "description": "Some description here"
 
     }, {
         "employer": "Charles Schwab",
         "title": "Client Services Intern",
         "location": "Phoenix,Arizona",
-        "dates": "June 2014 to Aug 2014"
+        "dates": "June 2014 to Aug 2014",
+        "description": "Some description here"
 
     }]
-}
+};
 
 var projects = {
 
@@ -50,7 +52,7 @@ var projects = {
         "description": "Information retrieval",
         "images": "url"
     }]
-}
+};
 
 
 var education = {
@@ -84,5 +86,70 @@ var bio = {
         "location": "Tempe,Arizona"
     },
     "skills": ["Java", "JavaScript", "PHP", "C#"],
-    "biopic": "url"
+    "biopic": "images/fry.jpg",
+    "message": "Hello, Welcome to my Website !"
+};
+
+
+
+
+
+function displayWork() {
+
+    for (i in work.jobs) {
+
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+        var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+        var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+        var formattedworkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+        var formattedworkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+        var formattedEmpTitle = formattedEmployer + formattedWorkTitle;
+        $(".work-entry:last").append(formattedEmpTitle);
+        $(".work-entry:last").append(formattedWorkDates);
+        $(".work-entry:last").append(formattedworkDescription);
+        $(".work-entry:last").append(formattedworkLocation);
+
+    }
+
 }
+
+function displayBio() {
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $(formattedName).insertBefore('#topContacts');
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $(formattedRole).insertBefore('#topContacts');
+
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+    $('#topContacts').append(formattedMobile);
+    $('#topContacts').append(formattedEmail);
+    $('#topContacts').append(formattedGithub);
+    $('#topContacts').append(formattedLocation);
+
+    var formattedbioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    $(formattedbioPic).insertBefore('#topContacts');
+    var formattedWelcomeMessage = HTMLWelcomeMsg.replace("%data%", bio.message);
+    $(formattedWelcomeMessage).insertAfter('#topContacts');
+
+    if (bio.skills.length > 0) {
+
+        $("#header").append(HTMLskillsStart);
+
+        var formattedSkill;
+
+        for (var i = 0; i < bio.skills.length; i++) {
+
+            formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+            $("#skills").append(formattedSkill);
+        }
+
+    }
+
+}
+
+displayBio();
+displayWork();
